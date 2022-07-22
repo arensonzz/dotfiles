db865a90c1c9caf8fadd720648a0a84340be9a6a
arensonz <echelon_erkan@hotmail.com>
2022-07-22 16:34:35 +0300

    Add scripts

---
+++ bin/change_system_theme.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# Switch between system wide dark and light mode with each execution
+
+dark_theme='Mint-Y-Dark-Teal'
+light_theme='Mint-Y-Teal'
+
+color_value="$(cat /tmp/system_theme_color 2>/dev/null)"
+
+# if 
+if [[ "$color_value" = "light" ]]; then
+    theme=$dark_theme
+    echo "dark" > /tmp/system_theme_color
+else
+    theme=$light_theme 
+    echo "light" > /tmp/system_theme_color
+fi
+
+
+gsettings set org.cinnamon.desktop.interface icon-theme $theme
+gsettings set org.cinnamon.desktop.interface gtk-theme $theme
+gsettings set org.cinnamon.desktop.wm.preferences theme $theme
+gsettings set org.mate.interface icon-theme  $theme
+gsettings set org.mate.interface gtk-theme  $theme
+gsettings set org.cinnamon.theme name  $theme
+
+gsettings set org.gnome.desktop.interface icon-theme $theme
+gsettings set org.gnome.desktop.interface gtk-theme $theme
---
+++ bin/immediate_screenshot.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+gnome-screenshot  -w -B -f /home/arensonz/Pictures/screenshots/$(date +"%Y-%m-%d_%H-%M-%S")_screenshot.png
